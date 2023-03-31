6ec7961af73d70ee1922593bb5ddbd2b786c11de
Vinicius Pinheiro <vinicius.pinheiro@yoti.com>
2023-03-31 15:47:10 +0100

    INF-29522: Add temperatures from Stats app

--- host/smc_darwin.h
+++ host/smc_darwin.h
@@ -3,47 +3,54 @@
 
 #include <IOKit/IOKitLib.h>
 
-#define AMBIENT_AIR_0          "TA0P"
-#define AMBIENT_AIR_1          "TA1P"
-#define CPU_0_DIODE            "TC0D"
-#define CPU_0_HEATSINK         "TC0H"
-#define CPU_0_PROXIMITY        "TC0P"
-#define ENCLOSURE_BASE_0       "TB0T"
-#define ENCLOSURE_BASE_1       "TB1T"
-#define ENCLOSURE_BASE_2       "TB2T"
-#define ENCLOSURE_BASE_3       "TB3T"
-#define GPU_0_DIODE            "TG0D"
-#define GPU_0_HEATSINK         "TG0H"
-#define GPU_0_PROXIMITY        "TG0P"
-#define HARD_DRIVE_BAY         "TH0P"
-#define MEMORY_SLOT_0          "TM0S"
-#define MEMORY_SLOTS_PROXIMITY "TM0P"
-#define NORTHBRIDGE            "TN0H"
-#define NORTHBRIDGE_DIODE      "TN0D"
-#define NORTHBRIDGE_PROXIMITY  "TN0P"
-#define THUNDERBOLT_0          "TI0P"
-#define THUNDERBOLT_1          "TI1P"
-#define WIRELESS_MODULE        "TW0P"
-#define CPU_EFFICIENCY_CORE_1  "TP09"
-#define CPU_EFFICIENCY_CORE_2  "TP0T"
-#define CPU_PERFORMANCE_CORE_1 "TP01"
-#define CPU_PERFORMANCE_CORE_2 "TP05"
-#define CPU_PERFORMANCE_CORE_3 "TP0D"
-#define CPU_PERFORMANCE_CORE_4 "TP0H"
-#define CPU_PERFORMANCE_CORE_5 "TP0L"
-#define CPU_PERFORMANCE_CORE_6 "TP0P"
-#define CPU_PERFORMANCE_CORE_7 "TP0X"
-#define CPU_PERFORMANCE_CORE_8 "TP0B"
-#define GPU_1                  "TG05"
-#define GPU_2                  "TG0D"
-#define GPU_3                  "TG0L"
-#define GPU_4                  "TG0T"
-#define AIRFLOW_LEFT           "TALP"
-#define AIRFLOW_RIGHT          "TARF"
-#define NAND                   "TH0X"
-#define BATTERY_1              "TB1T"
-#define BATTERY_2              "TB2T"
-#define AIRPORT                "TW0P"
+//Intel keys (Stats App)
+#define CPU_DIODE_FILTERED			"TC0F"
+#define CPU_PROXIMITY				"TC0P"
+#define CPU_CORE_1 					"TC1C" //or TC1c
+#define CPU_CORE_2 					"TC2C" //or TC2c
+#define CPU_CORE_3 					"TC3C" //or TC3c
+#define CPU_CORE_4 					"TC4C" //or TC4c
+#define CPU_CORE_5 					"TC5C" //or TC5c
+#define CPU_CORE_6 					"TC6C" //or TC6c
+#define GPU_INTEL_GRAPHICS			"TCGC"
+#define AIRPORT						"TW0P"
+//(Mac-exporter)
+#define MEMORY_SLOTS_PROXIMITY 		"TM0P"
+
+
+//M1 keys (Stats App)
+#define M1_CPU_EFFICIENCY_CORE_1	"Tp09"
+#define M1_CPU_EFFICIENCY_CORE_2	"Tp0T"
+#define M1_CPU_PERFORMANCE_CORE_1	"Tp01"
+#define M1_CPU_PERFORMANCE_CORE_2	"Tp05"
+#define M1_CPU_PERFORMANCE_CORE_3	"Tp0D"
+#define M1_CPU_PERFORMANCE_CORE_4	"Tp0H"
+#define M1_CPU_PERFORMANCE_CORE_5	"Tp0L"
+#define M1_CPU_PERFORMANCE_CORE_6	"Tp0P"
+#define M1_CPU_PERFORMANCE_CORE_7	"Tp0X"
+#define M1_CPU_PERFORMANCE_CORE_8	"Tp0b"
+#define M1_GPU_1					"Tg05"
+#define M1_GPU_2					"Tg0D"
+#define M1_GPU_3					"Tg0L"
+#define M1_GPU_4					"Tg0T"
+#define M1_NAND						"TH0x"
+#define M1_AIRPORT					"TW0P"
+
+//M2 keys (Stats App)
+#define M2_AIRPORT					"TW0P"
+#define M2_NAND						"TH0x"
+#define M2_DISK_1_B					"TH1B"
+#define M2_CPU_EFFICIENCY_CORE_1	"Tp05"
+#define M2_CPU_EFFICIENCY_CORE_2	"Tp0D"
+#define M2_CPU_EFFICIENCY_CORE_3	"Tp0j"
+#define M2_CPU_EFFICIENCY_CORE_4	"Tp0r"
+#define M2_CPU_PERFORMANCE_CORE_1	"Tp01"
+#define M2_CPU_PERFORMANCE_CORE_2	"Tp09"
+#define M2_CPU_PERFORMANCE_CORE_3	"Tp0f"
+#define M2_CPU_PERFORMANCE_CORE_4	"Tp0n"
+#define M2_GPU_1					"Tg0f"
+#define M2_GPU_2					"Tg0n"
+
 
 kern_return_t gopsutil_v3_open_smc(void);
 kern_return_t gopsutil_v3_close_smc(void);
